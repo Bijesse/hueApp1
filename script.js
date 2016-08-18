dedebbdd28de1319a309d8895cb7c6a89de9ef63
Bijesse <tom@bijesse.com>
2016-08-18 19:00:12 -0400

    moved repo

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 $(document).ready(function(){
-console.log("works");
-    var request = "http://192.168.1.2/api/OGIOGrBPRyn1AutXp3tLkZFIbMETdMaUCIBBkCL7";  
+//console.log("works");
+    var request = "http://192.168.1.2/api/OGIOGrBPRyn1AutXp3tLkZFIbMETdMaUCIBBkCL7";
    // $.get(request, function(response){
    //    console.log(response.lights[1].state.on);
    //  });
 
    // });
+
+
+  //  data: JSON.stringify(yourObj)
+  //  contentType: 'application/json',
+
    
   $.ajax(
   {
@@ -15,18 +20,19 @@ console.log("works");
         console.log(response.lights[1].state.bri);
 
       },
-      
-  // url: request,
-  // type: 'PUT',
-  // data: "/lights/1/state/on",
-  // success: function(data) {
-  //  alert("put stuff")
-  //   {"on":false};
-  // },
+
+  url: request + "/lights/4/state/on",
+  type: 'PUT',
+  datatype: 'json',
+  data: {"on":false},
+  success: function(data) {
+   console.log("put stuff")
+  },
+
+
       error: function (response) {
         console.log(response);
       }
     });
-
 
 });
