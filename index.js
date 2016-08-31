59abc6c9fb3db46e4b1b2eea5f8143b7dc782470
Bijesse <tom@bijesse.com>
2016-08-30 20:54:46 -0400

    added johnnyfive components

--- index.js
+++ index.js
@@ -19,3 +19,17 @@ app.listen(1337, function () {
 
 
 console.log("hi");
+
+// var Tessel = require("tessel-io");
+// var five = require("johnny-five");
+//
+// var board = new five.Board({
+//   io: new Tessel()
+// });
+//
+// board.on("ready", () => {
+//   var led = new five.Led("a5");
+//   var button = new five.Button("a2");
+//   button.on("press", () => led.on());
+//   button.on("release", () => led.off());
+// });
